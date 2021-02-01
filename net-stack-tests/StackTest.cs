3595d267c2bb07a411ae1f10d2e1922f51418932
Daniel Hopson <hdhopson88@yahoo.com>
2021-02-01 14:31:19 -0800

    Initial Commit

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -15,6 +16,118 @@ namespace net_stack_tests
     }
 
     // implement tests
+    [TestMethod]
+    public void IsEmptyReturnsTrue()
+    {
+      var sut = stack.IsEmpty();
+      
+      Assert.IsTrue(sut);
+    }
 
+    [TestMethod]
+    public void IsEmptyReturnsFalseAfterPush()
+    {
+      stack.Push("test1");
+
+      var sut = stack.IsEmpty();
+      
+      Assert.IsFalse(sut);
+    }
+
+    [TestMethod]
+    public void PeekReturnsNullWhenStackIsEmpty()
+    {
+      var sut = stack.Peek();
+      
+      Assert.IsNull(sut);
+    }
+
+    [TestMethod]
+    public void PeekReturnsMostRecentlyPushedValue()
+    {
+      stack.Push("test1");
+
+      var sut = stack.Peek();
+      
+      Assert.AreEqual("test1", sut);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(EmptyStackException))]
+    public void PopThrowsEmptyStackExceptionWhenStackEmpty()
+    {
+      stack.Pop();
+    }
+
+    [TestMethod]
+    public void PopReturnsMostRecentPushedValue()
+    {
+      stack.Push(1);
+
+      var sut = stack.Pop();
+
+      Assert.AreEqual(1, sut);
+    }
+
+    [TestMethod]
+    public void PeekReturnsNullAfterPopIsCalled()
+    {
+      stack.Push(1);
+      stack.Pop();
+
+      var sut = stack.Peek();
+
+      Assert.IsNull(sut);
+    }
+
+    [TestMethod]
+    public void IsEmptyReturnsTrueAfterPopIsCalled()
+    {
+      stack.Push(1);
+      stack.Pop();
+
+      var sut = stack.IsEmpty();
+
+      Assert.IsTrue(sut);
+    }
+
+    [TestMethod]
+    public void AfterTwoPushesPopReturns2ndPushedValue_PeekReturns1stPushedValue()
+    {
+      stack.Push(1);
+      stack.Push(2);
+
+      var sut1 = stack.Pop();
+      var sut2 = stack.Peek();
+
+      Assert.AreEqual(2, sut1);
+      Assert.AreEqual(1, sut2);
+    }
+
+    [TestMethod]
+    public void ContainsReturnsTrueIfValuePresent()
+    {
+      stack.Push(1);
+      stack.Push(2);
+      stack.Push(3);
+      stack.Push(4);
+
+      var sut = stack.Contains(2);
+
+      Assert.IsTrue(sut);
+    }
+
+    [TestMethod]
+    public void ContainsReturnsFalseIfValueNotPresent()
+    {
+      stack.Push(1);
+      stack.Push(2);
+      stack.Push(3);
+      stack.Push(4);
+
+      var sut = stack.Contains(5);
+
+      Assert.IsFalse(sut);
+    }
   }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,107 @@
 ﻿using System;
+using net_stack.model.exceptions;
+
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Node
+    {
+        public Object data;
+        public Node nextNode;
+
+        public Node()
+        {
+            data = null;
+            nextNode = null;
+        }
+    }
+
+    public class Stack
+    {
+        // Implement code
+        Node head;
+        Object _obj;
+
+        public Stack()
+        {
+            head = null;
+            _obj = null;
+        }
+
+        public bool IsEmpty()
+        {
+            // 1.) returns is empty equal to true
+            // return true
+
+            // 2.) returns is empty equal to false after push
+            // return _obj == null;
+
+            // 9.) Implement Linked list
+            return head == null;
+        }
+
+        public void Push(Object obj)
+        {
+            // 2.) returns is empty equal to false after push
+            // _obj = obj;
+
+            var newNode = new Node();
+            newNode.data = obj;
+            newNode.nextNode = head;
+            head = newNode;
+        }
+
+        public Object Peek()
+        {
+            // 3.) & 4.) returns null or most recently pushed value
+            // return _obj;
+
+            if (head == null)
+            {
+                return null;
+            }
+
+            return head.data;
+        }
+
+        public Object Pop()
+        {
+            if (IsEmpty())
+            {
+                // 5.) Pop throws exception is stack is empty
+                throw new EmptyStackException("The stack is empty.");
+            }
+
+            // 6.) Returns the most recently pushed value
+            // return _obj;
+
+            // 7.) Peek returns null after pop is called
+
+            // 9.) Implement linked list
+            var popped = head.data;
+            head = head.nextNode;
+            return popped;
+        }
+
+        public bool Contains(Object obj)
+        {
+            if (IsEmpty())
+            {
+                throw new EmptyStackException("The stack is empty.");
+            }
+
+            Node temp = head;
+
+            while (temp != null)
+            {
+                if (temp.data.Equals(obj))
+                {
+                    return true;
+                }
+                
+                temp = temp.nextNode;
+            }
+
+            return false;
+        }
+    }
 }
